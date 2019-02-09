7991b30ef84e0d4cefb749c177c28a260fcd3850
sbimochan <bmochan@gmail.com>
2019-02-09 12:29:34 +0545

    React Animate number v1.0.0

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class AnimateNumbers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      finalNumber: parseInt(props.to) || null,
+      initialNumber: parseInt(props.from) || null,
+      delay: parseInt(props.delay) || 25,
+    };
+  }
+
+  runEngine() {
+    if (this.state.initialNumber < this.state.finalNumber) {
+      setTimeout(() => {
+        this.setState({
+          initialNumber: this.state.initialNumber + 1
+        });
+        this.runEngine();
+      }, this.state.delay);
+    }
+  };
+
+  componentDidMount() {
+    this.runEngine();
+  }
+
+  render() {
+    return this.state.initialNumber;
+  }
+}
+
+export default AnimateNumbers;
