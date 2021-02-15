5009cd293b5ef9a1a40966fcca69f40df1694d87
Bimochan Shrestha <bmochan@gmail.com>
2021-02-15 15:11:48 +0545

    Add fallback

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
-export default NumberScroller = ({ to, from, delay }) => {
+export default NumberScroller = ({ to, from, delay, fallback }) => {
 	const [finalNumber, setFinalNumber] = useState(parseInt(to, RADIX) || 0);
 	const [initialNumber, setInitialNumber] = useState(parseInt(from, RADIX) || 0);
 	const [delay, setDelay] = useState(parseInt(delay, RADIX) || 25);
 
   useEffect = (() => {
     runEngine();
-  },[to])
+  },[to]);
   
 	const runEngine = () => {
 		if (initialNumber < finalNumber) {
@@ -18,5 +18,11 @@ export default NumberScroller = ({ to, from, delay }) => {
 		}
 	};
 
-	return initialNumber;
+	return (
+    if (!fallback) {
+      return initialNumber;
+    }
+
+    return initialNumber > 0 ? initialNumber : fallback;
+  );
 };
