791510aff2c7e204263fadd76b1f8a25fb277bc1
Bimochan Shrestha <bmochan@gmail.com>
2021-02-15 15:07:23 +0545

    Rewrite to react hooks

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-class NumberScroller extends React.Component {
-  constructor(props) {
-    super(props);
-    const RADIX = 10;
-    this.state = {
-      finalNumber: parseInt(props.to, RADIX) || 0,
-      initialNumber: parseInt(props.from, RADIX) || 0,
-      delay: parseInt(props.delay, RADIX) || 25
-    };
-  }
+export default NumberScroller = ({ to, from, delay }) => {
+	const [finalNumber, setFinalNumber] = useState(parseInt(to, RADIX) || 0);
+	const [initialNumber, setInitialNumber] = useState(parseInt(from, RADIX) || 0);
+	const [delay, setDelay] = useState(parseInt(delay, RADIX) || 25);
 
-  runEngine() {
-    if (this.state.initialNumber < this.state.finalNumber) {
-      setTimeout(() => {
-        this.setState({
-          initialNumber: this.state.initialNumber + 1
-        });
-        this.runEngine();
-      }, this.state.delay);
-    }
-  }
+  useEffect = (() => {
+    runEngine();
+  },[to])
+  
+	const runEngine = () => {
+		if (initialNumber < finalNumber) {
+			setTimeout(() => {
+        setInitialNumber(initialNumber++)
+				runEngine();
+			}, delay);
+		}
+	};
 
-  componentDidMount() {
-    this.runEngine();
-  }
-
-  render() {
-    return this.state.initialNumber;
-  }
-}
-
-export default NumberScroller;
+	return initialNumber;
+};
