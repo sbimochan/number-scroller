5d3b71dd80cbb1bb1c26a4d1edc4f76af59af84b
Kevin Vandy <kevinvandy656@gmail.com>
2021-02-21 21:29:17 -0600

    delete old and add delay and renderFrequency props

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
 import React, { useState, useEffect, useRef, FC } from 'react';
 
 export interface NumberScrollerProps {
+  /**
+   * Time in milliseconds to delay the start of the number scroll
+   */
   delay?: number;
+  /**
+   * Provide a default value to display if to is ever undefined
+   */
   fallback?: number;
+  /**
+   * Provide the starting number before number scroll animation
+   */
   from?: number;
+  /**
+   * provide options for converting number to a localized string (such as currency)
+   */
   localeStringProps?: [string, any];
+  /**
+   * Instead of a timeout, specify the exact number of milliseconds between each scroll
+   */
+  renderFrequency?: number;
+  /**
+   * Customize the increment amount between each scroll
+   */
   step?: number;
+  /**
+   * How long it should take for the number to reach its 'to' value
+   */
   timeout?: number;
-  to: number;
+  /**
+   * The value that it should scroll to
+   */
+  to?: number;
 }
 
 export const NumberScroller: FC<NumberScrollerProps> = ({
-  delay,
+  delay = 0,
+  renderFrequency,
   fallback = 0,
   from = 0,
   localeStringProps,
-  step = 1,
+  step = 3,
   timeout = 1000,
   to,
 }) => {
@@ -27,16 +53,16 @@ export const NumberScroller: FC<NumberScrollerProps> = ({
       if (currentNumber !== to) {
         setTimeout(() => {
           setCurrentNumber(
-            currentNumber < to
+            currentNumber < (to ?? 0)
               ? (currentNumber += Math.abs(step))
               : (currentNumber -= Math.abs(step))
           );
           runEngine();
-        }, delay || timeout / Math.max(initialDifference.current, 1) || 1);
+        }, renderFrequency || timeout / initialDifference.current || 1);
       }
     };
-    initialDifference.current = Math.abs(currentNumber - to);
-    runEngine();
+    initialDifference.current = Math.abs(currentNumber - (to ?? 0)) || 1;
+    setTimeout(() => runEngine(), delay);
   }, [to]);
 
   const newNumber = currentNumber ?? fallback;
