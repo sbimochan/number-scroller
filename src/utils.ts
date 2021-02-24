3d5f6940ba5194e935984358053910b69c56b834
Biplap Bhattarai <bhattaraib58@gmail.com>
2021-02-24 13:02:40 +0545

    Create util to get next step value

--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,30 @@ export function getRoundedFloatWithPrecision(num: number, precision = 2) {
 
   return Math.round((num + Number.EPSILON) * ROUND_UPTO) / ROUND_UPTO;
 }
+
+/**
+ * Get Next Step Value for the number change.
+ *
+ * @param {number} currentStepValue
+ * @param {number} toValue
+ * @param {number} step
+ *
+ * @return {number}
+ */
+export const getNextStepValue = (
+  currentStepValue: number,
+  toValue: number,
+  step: number
+) => {
+  if (currentStepValue < (toValue ?? 0)) {
+    return (currentStepValue += Math.min(
+      Math.abs(step),
+      (toValue ?? 0) - currentStepValue
+    ));
+  } else {
+    return (currentStepValue -= Math.min(
+      Math.abs(step),
+      (toValue ?? 0) + currentStepValue
+    ));
+  }
+};
